7bc0fa9af097fbabb06458e76cf0dc2332c9835b
Swarthon <swarthon.gokan@gmail.com>
2017-09-20 18:27:59 +0200

    Adding color and answers support to ActivityResult (crashing during runtime

--- app/src/main/java/org/allemand_euro/verbesforts/ResultRightActivity.java
+++ app/src/main/java/org/allemand_euro/verbesforts/ResultRightActivity.java
@@ -16,11 +16,25 @@ import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+import android.graphics.Color;
+
 public class ResultRightActivity extends AppCompatActivity {
+        protected void initTextView(TextView textView, String givenAnswer, Vector<String> answers) {
+                Boolean right = false;
+                for(int i = 0; i < answers.size(); i++) {
+                        if(answers.get(i).equalsIgnoreCase(givenAnswer))
+                                right = true;
+                        textView.setText(answers.get(i));
+                }
+                if(right)
+                        textView.setTextColor(getResources().getColor(Color.GREEN));
+                if(!right)
+                        textView.setTextColor(getResources().getColor(Color.RED));
+        }
         @Override
         protected void onCreate(Bundle savedInstanceState) {
                 super.onCreate(savedInstanceState);
-                setContentView(R.layout.activity_resultright);
+                setContentView(R.layout.activity_result);
 
                 final TextView infinitif = (TextView) findViewById(R.id.infinitif_result);
                 final TextView preterit = (TextView) findViewById(R.id.preterit_result);
@@ -35,5 +49,12 @@ public class ResultRightActivity extends AppCompatActivity {
 
                 String givenAnswers[] = getIntent().getExtras().getStringArray("givenForms");
                 int givenFormType = getIntent().getExtras().getInt("givenFormType");
+
+                initTextView(infinitif, givenAnswers[0], answers.get(0));
+                initTextView(preterit, givenAnswers[1], answers.get(1));
+                initTextView(participe, givenAnswers[2], answers.get(2));
+                initTextView(troisiemePersonne, givenAnswers[3], answers.get(3));
+                initTextView(traduction, givenAnswers[4], answers.get(4));
+                initTextView(aux, givenAnswers[5], answers.get(5));
         }
 }
