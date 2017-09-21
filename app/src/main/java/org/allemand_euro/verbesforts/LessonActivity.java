84dbe55abc26ea3f231d6db4bd545f44ff5b136e
Swarthon <swarthon.gokan@gmail.com>
2017-09-21 15:47:02 +0200

    Correcting crash and updating UI

--- app/src/main/java/org/allemand_euro/verbesforts/LessonActivity.java
+++ app/src/main/java/org/allemand_euro/verbesforts/LessonActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Vector;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
@@ -27,57 +28,32 @@ public class LessonActivity extends AppCompatActivity {
                 super.onCreate(savedInstanceState);
                 setContentView(R.layout.activity_lesson);
                 
-                // get the listview
                 expListView = (ExpandableListView) findViewById(R.id.lesson_list);
- 
-                // preparing list data
                 prepareListData();
- 
+                
                 listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
- 
-                // setting list adapter
                 expListView.setAdapter(listAdapter);
         }
  
-        /*
-         * Preparing the list data
-         */
         private void prepareListData() {
                 listDataHeader = new ArrayList<String>();
                 listDataChild = new HashMap<String, List<String>>();
- 
+
+                Vector<Verb> verbs = Loader.GetSingleton().mVerbs;
                 // Adding child data
-                listDataHeader.add("Top 250");
-                listDataHeader.add("Now Showing");
-                listDataHeader.add("Coming Soon..");
- 
-                // Adding child data
-                List<String> top250 = new ArrayList<String>();
-                top250.add("The Shawshank Redemption");
-                top250.add("The Godfather");
-                top250.add("The Godfather: Part II");
-                top250.add("Pulp Fiction");
-                top250.add("The Good, the Bad and the Ugly");
-                top250.add("The Dark Knight");
-                top250.add("12 Angry Men");
- 
-                List<String> nowShowing = new ArrayList<String>();
-                nowShowing.add("The Conjuring");
-                nowShowing.add("Despicable Me 2");
-                nowShowing.add("Turbo");
-                nowShowing.add("Grown Ups 2");
-                nowShowing.add("Red 2");
-                nowShowing.add("The Wolverine");
- 
-                List<String> comingSoon = new ArrayList<String>();
-                comingSoon.add("2 Guns");
-                comingSoon.add("The Smurfs 2");
-                comingSoon.add("The Spectacular Now");
-                comingSoon.add("The Canyons");
-                comingSoon.add("Europa Report");
- 
-                listDataChild.put(listDataHeader.get(0), top250); // Header, Child data
-                listDataChild.put(listDataHeader.get(1), nowShowing);
-                listDataChild.put(listDataHeader.get(2), comingSoon);
+                for(int i = 0; i < verbs.size(); i++) {
+                        listDataHeader.add(verbs.get(i).mForms.get(0).get(0));
+                        List<String> details = new ArrayList<String>();
+                        for(int j = 0; j < verbs.get(i).mForms.size(); j++) {
+                                String caseVerb = Questions.FormToWord(j) + " : ";
+                                for(int k = 0; k < verbs.get(i).mForms.get(j).size(); k++) {
+                                        caseVerb += verbs.get(i).mForms.get(j).get(k);
+                                        if(k < verbs.get(i).mForms.get(j).size() - 1)
+                                                caseVerb += ", ";
+                                }
+                                details.add(caseVerb);
+                        }
+                        listDataChild.put(listDataHeader.get(i), details);
+                }
         }
 }
