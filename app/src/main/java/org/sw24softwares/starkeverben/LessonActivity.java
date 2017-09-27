927899c8ef5198d30adc3c6c661da5acc3d1fb85
Swarthon <swarthon.gokan@gmail.com>
2017-09-27 19:49:35 +0200

    Adding search bar, unstable

--- app/src/main/java/org/sw24softwares/starkeverben/LessonActivity.java
+++ app/src/main/java/org/sw24softwares/starkeverben/LessonActivity.java
@@ -7,6 +7,10 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Vector;
+import java.util.Set;
+
+import android.content.Intent;
+import android.app.SearchManager;
 
 import android.widget.ExpandableListView;
 
@@ -20,33 +24,48 @@ public class LessonActivity extends AppCompatActivity {
         protected void onCreate(Bundle savedInstanceState) {
                 super.onCreate(savedInstanceState);
                 setContentView(R.layout.activity_lesson);
+
+                onSearchRequested();
                 
                 expListView = (ExpandableListView) findViewById(R.id.lesson_list);
-                prepareListData();
                 
-                listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
-                expListView.setAdapter(listAdapter);
+                // Get the intent, verify the action and get the query
+                Intent intent = getIntent();
+                if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+                        String query = intent.getStringExtra(SearchManager.QUERY);
+                        search(query);
+                }
         }
  
-        private void prepareListData() {
+        private void prepareListData(String search_word) {
                 listDataHeader = new ArrayList<String>();
                 listDataChild = new HashMap<String, List<String>>();
 
                 Vector<Verb> verbs = Loader.GetSingleton().mVerbs;
                 // Adding child data
                 for(int i = 0; i < verbs.size(); i++) {
-                        listDataHeader.add(verbs.get(i).mForms.get(0).get(0));
+                        Boolean contain = false;
                         List<String> details = new ArrayList<String>();
                         for(int j = 0; j < verbs.get(i).mForms.size(); j++) {
                                 String caseVerb = Questions.FormToWord(j) + " : ";
                                 for(int k = 0; k < verbs.get(i).mForms.get(j).size(); k++) {
+                                        if(verbs.get(i).mForms.get(j).get(k).contains(search_word))
+                                                contain = true;
                                         caseVerb += verbs.get(i).mForms.get(j).get(k);
                                         if(k < verbs.get(i).mForms.get(j).size() - 1)
                                                 caseVerb += ", ";
                                 }
                                 details.add(caseVerb);
                         }
-                        listDataChild.put(listDataHeader.get(i), details);
+                        if(contain) {
+                                listDataHeader.add(verbs.get(i).mForms.get(0).get(0));
+                                listDataChild.put(listDataHeader.get(i), details);
+                        }
                 }
+        }
+        private void search(String word) {
+                prepareListData(word);
+                listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
+                expListView.setAdapter(listAdapter);
         }
 }
