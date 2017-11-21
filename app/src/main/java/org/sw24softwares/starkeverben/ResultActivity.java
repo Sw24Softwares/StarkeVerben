47d1ca2cd31b35e79f09d3093e6df645e8541bc9
Swarthon <swarthon.gokan@gmail.com>
2017-11-21 21:39:08 +0100

    Clean ResultActivity

--- app/src/main/java/org/sw24softwares/starkeverben/ResultActivity.java
+++ app/src/main/java/org/sw24softwares/starkeverben/ResultActivity.java
@@ -20,69 +20,49 @@ import java.util.Vector;
 
 public class ResultActivity extends AppCompatActivity {
         int mMarks[] = null;
-        DatabaseHelper mDatabaseHelper;
 
-        protected void saveMarkDialog(final int theMarks[]) {
+        protected void saveMarkDialog() {
                 AlertDialog alertDialog = new AlertDialog.Builder(ResultActivity.this).create();
-
 		alertDialog.setTitle(getString(R.string.save));
                 alertDialog.setMessage(getString(R.string.save_message));
+
                 alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, getString(R.string.yes),
-                
 		new DialogInterface.OnClickListener() {
                         public void onClick(DialogInterface dialog, int which) {
-                            Intent intent = new Intent(ResultActivity.this, MainActivity.class);
-
-                            String marks = new String();
-                            int totalPercent = 0;
-
-                            for(int i = 0; i < theMarks.length; i++) {
-                                marks += String.valueOf(theMarks[i]) + ' ';
-                                totalPercent += theMarks[i];
-                            }
-
-                            totalPercent = Math.round(totalPercent * 100 / (theMarks.length * 5));
-                            int onTwenty = Math.round(totalPercent * 2 / 10);
-
-                            ContentValues contentValues = new ContentValues();
-                            contentValues.put(DatabaseHelper.COLUMN_1, new SimpleDateFormat("dd/MM/yyyy '" + getString(R.string.at) + "' HH:mm : '" + String.valueOf(totalPercent) + "% - (" + onTwenty + "/20)'").format(new Date()));
-                            contentValues.put(DatabaseHelper.COLUMN_2, marks);
-                            mDatabaseHelper.addData(contentValues);
-			    
-			    startActivity(intent);
+                                saveMark();
                         }
-                    });
+                });
 
                 alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, getString(R.string.no),
-                    new DialogInterface.OnClickListener() {
+                new DialogInterface.OnClickListener() {
                         public void onClick(DialogInterface dialog, int which) {
-                            Intent intent = new Intent(ResultActivity.this, MainActivity.class);
-                            startActivity(intent);
+                                Intent intent = new Intent(ResultActivity.this, MainActivity.class);
+                                startActivity(intent);
                         }
-                    });
-
+                });
                 alertDialog.show();
         }
 
-        protected void saveMark(final int theMarks[]) {
-		Intent intent = new Intent(ResultActivity.this, MainActivity.class);
-
+        protected void saveMark() {
 		String marks = new String();
 		int totalPercent = 0;
 
-		for(int i = 0; i < theMarks.length; i++) {
-                        marks += String.valueOf(theMarks[i]) + ' ';
-                        totalPercent += theMarks[i];
+		for(int i = 0; i < mMarks.length; i++) {
+                        marks += String.valueOf(mMarks[i]) + ' ';
+                        totalPercent += mMarks[i];
 		}
 
-		totalPercent = Math.round(totalPercent * 100 / (theMarks.length * 5));
+		totalPercent = Math.round(totalPercent * 100 / (mMarks.length * 5));
 		int onTwenty = Math.round(totalPercent * 2 / 10);
 
 		ContentValues contentValues = new ContentValues();
 		contentValues.put(DatabaseHelper.COLUMN_1, new SimpleDateFormat("dd/MM/yyyy '" + getString(R.string.at) + "' HH:mm : '" + String.valueOf(totalPercent) + "% - (" + onTwenty + "/20)'").format(new Date()));
 		contentValues.put(DatabaseHelper.COLUMN_2, marks);
-		mDatabaseHelper.addData(contentValues);
 
+                DatabaseHelper databaseHelper = new DatabaseHelper(this);
+		databaseHelper.addData(contentValues);
+
+                Intent intent = new Intent(ResultActivity.this, MainActivity.class);
 		startActivity(intent);
         }
 
@@ -105,14 +85,13 @@ public class ResultActivity extends AppCompatActivity {
                 super.onCreate(savedInstanceState);
                 setContentView(R.layout.activity_result);
 
-                final TextView infinitif = (TextView) findViewById(R.id.infinitif_result);
-                final TextView preterit = (TextView) findViewById(R.id.preterit_result);
-                final TextView participe = (TextView) findViewById(R.id.participe_result);
-                final TextView troisiemePersonne = (TextView) findViewById(R.id.troisieme_personne_result);
-                final TextView traduction = (TextView) findViewById(R.id.traduction_result);
-                final TextView aux = (TextView) findViewById(R.id.auxiliaire_result);
-
-                mDatabaseHelper = new DatabaseHelper(this);
+                final Vector<TextView> textViews = new Vector<TextView>();
+                textViews.addElement((TextView) findViewById(R.id.infinitive_result));
+                textViews.addElement((TextView) findViewById(R.id.preterit_result));
+                textViews.addElement((TextView) findViewById(R.id.participe_result));
+                textViews.addElement((TextView) findViewById(R.id.third_person_result));
+                textViews.addElement((TextView) findViewById(R.id.translation_result));
+                textViews.addElement((TextView) findViewById(R.id.auxiliary_result));
 
                 Vector<Vector<String>> answers = new Vector<Vector<String>>();
                 for(int i = 0; i < 6; i++)
@@ -121,10 +100,7 @@ public class ResultActivity extends AppCompatActivity {
                 String givenAnswers[] = getIntent().getExtras().getStringArray("givenAnswers");
                 int givenFormType = getIntent().getExtras().getInt("givenFormType");
 	
-		final Boolean dialog = getIntent().getExtras().getBoolean("dialog");		//String to see if the user has took more of 20 seconds to complete each test (outputs the dialog if not)
-
-                final int total = getIntent().getExtras().getInt("total");
-                int marks[] = getIntent().getExtras().getIntArray("marks");
+                final int marks[] = getIntent().getExtras().getIntArray("marks");
                 mMarks = Arrays.copyOf(marks, marks.length +1);
 
                 if(givenAnswers[5].equals("sein"))	givenAnswers[5] = "ist";
@@ -132,33 +108,31 @@ public class ResultActivity extends AppCompatActivity {
                 if(answers.get(5).contains("sein"))	answers.set(5, new Vector(Arrays.asList("ist")));
                 else                                    answers.set(5, new Vector(Arrays.asList("hat")));
 
-                initTextView(infinitif, givenAnswers[0], (Vector<String>)answers.get(0).clone(), givenFormType != 0);
-                initTextView(preterit, givenAnswers[1], (Vector<String>)answers.get(1).clone(), givenFormType != 1);
-                initTextView(participe, givenAnswers[2], (Vector<String>)answers.get(2).clone(), givenFormType != 2);
-                initTextView(troisiemePersonne, givenAnswers[3], (Vector<String>)answers.get(3).clone(), givenFormType != 3);
-                initTextView(traduction, givenAnswers[4], (Vector<String>)answers.get(4).clone(), givenFormType != 4);
-                initTextView(aux, givenAnswers[5], (Vector<String>)answers.get(5).clone(), givenFormType != 5);
+                // Init the TextViews
+                for(int i = 0; i < textViews.size(); i++)
+                        initTextView(textViews.get(i), givenAnswers[i], (Vector<String>)answers.get(i).clone(), givenFormType != i);
 
-                Button arreter = (Button) findViewById(R.id.arreter);
-                Button continuer = (Button) findViewById(R.id.continuer);
 
-                arreter.setOnClickListener(new View.OnClickListener() {
+                Button finish = (Button) findViewById(R.id.finish);
+                finish.setOnClickListener(new View.OnClickListener() {
                                 @Override
                                 public void onClick(View view) {
-					if(dialog)
-						saveMarkDialog(mMarks);
+                                        // Whether open the dialog or not
+					if(getIntent().getExtras().getBoolean("dialog"))
+						saveMarkDialog();
 					else
-						saveMark(mMarks);
+						saveMark();
                                 }
                 });
-
-                continuer.setOnClickListener(new View.OnClickListener() {
+                
+                Button go_on = (Button) findViewById(R.id.go_on);
+                go_on.setOnClickListener(new View.OnClickListener() {
                                 @Override
                                 public void onClick(View view) {
                                         Intent intent = new Intent(ResultActivity.this, TestActivity.class);
-                                        intent.putExtra("total", total + 1);
+                                        intent.putExtra("total", getIntent().getExtras().getInt("total") + 1);
                                         intent.putExtra("marks", mMarks);
-					intent.putExtra("dialog", dialog);
+					intent.putExtra("dialog", getIntent().getExtras().getBoolean("dialog"));
                                         startActivity(intent);
                                 }
                         });
