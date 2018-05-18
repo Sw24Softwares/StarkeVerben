dea43c4d579b3209a72fa7613033cbafa9e4f2fe
Louis Vanhaelewyn <swarthon.gokan@gmail.com>
2018-05-18 23:01:18 +0200

    Print warning when clang-format is not installed

--- format-code.sh
+++ format-code.sh
@@ -1,2 +1,7 @@
 #! /bin/sh
-find app/src/main -iname "*.java" -exec clang-format -i {} \;
+if command -v clang-format >/dev/null 2>&1
+then
+    find app/src/main -iname "*.java" -exec clang-format -i {} \;
+else
+    echo "Could not format code. Please install clang-format"
+fi
