a5f34b5e816232585c2c790ff0d523e134d8b376
Hamza Parnica <hparnica@gmail.com>
2019-01-12 23:06:06 +0100

    Modify icon and add prettier splash screen
    Android splash screen is added according this: https://plus.google.com/+AndroidDevelopers/posts/Z1Wwainpjhd

--- app/src/main/java/org/sw24softwares/starkeverben/MainActivity.java
+++ app/src/main/java/org/sw24softwares/starkeverben/MainActivity.java
@@ -60,6 +60,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
